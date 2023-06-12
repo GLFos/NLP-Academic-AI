dbc97cd84d691dce8fb934c52680873b6b10923e
Garrison Foster <garrisonlukefoster@outlook.com>
2023-06-12 11:32:52 -0700

    Added ability to summarize larger texts

--- bart.py
+++ bart.py
@@ -7,10 +7,31 @@ class BARTSummarizer:
         self.tokenizer = BartTokenizer.from_pretrained(model_name)
 
     def summarize(self, text):
-        inputs = self.tokenizer.encode_plus(text, truncation=True, max_length=1024, padding="longest", return_tensors="tf")
-        input_ids = tf.cast(inputs["input_ids"], dtype=tf.int32)
-        summary_ids = self.model.generate(input_ids, num_beams=4, max_length=150, early_stopping=True)
-        summary = self.tokenizer.decode(summary_ids.numpy()[0], skip_special_tokens=True)
-        return summary
+        try:
+            inputs = self.tokenizer.encode_plus(text, truncation=True, max_length=1024, padding="longest", return_tensors="tf")
+            input_ids = tf.cast(inputs["input_ids"], dtype=tf.int32)
+            summary_ids = self.model.generate(input_ids, num_beams=4, max_length=150, early_stopping=True)
+            summary = self.tokenizer.decode(summary_ids.numpy()[0], skip_special_tokens=True)
+            return summary
+        except Exception as e:
+            print(f"An error has occured during summarization: {str(e)}")
+            return None
+    
+    def summarize_large_text(self, text):
+        chunks = [text[i : i + 1024] for i in range(0, len(text), 1024)]
+        summaries = []
+
+        for chunk in chunks:
+            try:
+                summary = self.summarize(chunk)
+                if summary:
+                    summary.append(summaries)
+            except Exception as e:
+                print(f"An error has occured during summarization: {str(e)}")
+                return None
+        
+        full_summary = " ".join(summaries)
+        return full_summary
+
 
 

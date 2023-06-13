0ee8d83d94de70081d04ae2b2db40c0e6bd11489
Garrison Foster <garrisonlukefoster@outlook.com>
2023-06-13 15:38:58 -0700

    Added a keyword extractor

---
+++ keywords.py
@@ -0,0 +1,18 @@
+import yake
+
+language = "en"
+max_ngram_size = 2
+deduplication_threshold = 0.9
+deduplication_algo = "seqm"
+numOfKeywords = 4
+
+class Custom_Keyword_Extract:
+    def __init__(self):
+        self.custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, dedupFunc=deduplication_algo, top=numOfKeywords, features=None)
+    
+    def extract_keywords(self, text):
+        keywords = self.custom_kw_extractor.extract_keywords(text)
+        return [(kw, score) for kw, score in keywords]
+
+
+
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from article import Article
 from bart import BARTSummarizer
+from keywords import Custom_Keyword_Extract
 
 summarizer = BARTSummarizer()
+keyword_extractor = Custom_Keyword_Extract()
 
-article_content = " "
+article_content = "https://apnews.com/article/pandemic-fraud-waste-billions-small-business-labor-fb1d9a9eb24857efbe4611344311ae78"
 
 article = Article(article_content)
 article.fetch_text()
@@ -12,3 +14,6 @@ summary = summarizer.summarize(content)
 
 print(summary)
 
+keywords = keyword_extractor.extract_keywords(content)
+for keyword, score in keywords:
+    print(keyword, score)

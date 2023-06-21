957bda98e64bb44383e8733051116a6b68d730bc
Garrison Foster <garrisonlukefoster@outlook.com>
2023-06-21 15:21:28 -0700

    Added requirements and an entry point

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from article import Article
-from bart import BARTSummarizer
-from keywords import Custom_Keyword_Extract
+from prototype import main
 
-# This compiles all three of the main classes
-
-summarizer = BARTSummarizer()
-keyword_extractor = Custom_Keyword_Extract()
-
-article_content = input("Enter your URL, file, or text: ")
-
-article = Article(article_content)
-article.fetch_text()
-content = article.get_text()  
-summary = summarizer.summarize(content)
-
-print(summary)
-
-keywords = keyword_extractor.extract_keywords(content)
-for keyword, score in keywords:
-    print(keyword, score)
+if __name__ == "__main__":
+    main()

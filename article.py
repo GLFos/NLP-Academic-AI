10978f99cab3f4db70d86f0ee0e6fd6b11602292
Garrison Foster <garrisonlukefoster@outlook.com>
2023-06-30 15:21:17 -0700

    Efficiency for websites and ability to read .docx

--- article.py
+++ article.py
@@ -1,6 +1,7 @@
 import requests
 from readability import Document
 from bs4 import BeautifulSoup
+from docx import Document
 import concurrent.futures
 
 max_cores = 2
@@ -22,34 +23,45 @@ class Article:
         return self.source.startswith("http") or self.source.startswith("https")
 
     def is_file(self):
-        return isinstance(self.source, str) and self.source.endswith(".txt")
+        return isinstance(self.source, str) and (self.source.endswith(".txt") or self.source.endswith(".docx"))
+
+    def process_html_content(self, html_content):
+        soup = BeautifulSoup(html_content, 'html.parser')
+        self.text = self.extract_article_text(soup)
+
+    def extract_article_text(self, soup):
+        return ""
 
     def fetch_text_from_url(self):
         response = requests.get(self.source)
         html_content = response.text
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=max_cores) as executor:
-            futures = [
-                executor.submit(self.process_html_content, html_content),
-                executor.submit(self.extract_article_text, html_content)
-            ]
-            concurrent.futures.wait(futures)
+            future = executor.submit(self.process_html_content, html_content)
+            concurrent.futures.wait([future])
 
-            processed_html_content = [future.result() for future in futures]
-
-        document = Document(processed_html_content)
-        article_html = document.summary()
-
-        soup = BeautifulSoup(article_html, "html.parser")
-        article_text = soup.get_text(separator=" ")
+        article_text = self.extract_text_from_html(html_content)
         self.set_text(article_text)
 
     def fetch_text_from_file(self):
-        with open(self.source, "r", encoding="utf-8") as file:
-            self.set_text(file.read())
+        if self.source.endswith(".txt"):
+            with open(self.source, "r", encoding="utf-8") as file:
+                self.set_text(file.read())
+        elif self.source.endswith(".docx"):
+            document = Document(self.source)
+            paragraphs = [paragraph.text for paragraph in document.paragraphs]
+            self.set_text("\n".join(paragraphs))
+
+    def extract_text_from_html(self, html):
+        document = Document(html)
+        article_html = document.summary()
+        soup = BeautifulSoup(article_html, "html.parser")
+        article_text = soup.get_text(separator=" ")
+        return article_text
 
     def set_text(self, text):
         self.text = text
 
     def get_text(self):
         return self.text
+
